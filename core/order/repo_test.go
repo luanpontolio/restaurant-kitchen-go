4a93e5dc016f100518b2d0f0550521fbecd8489d
Luan Silveira Pontolio <luan.pontolio@gmail.com>
2021-03-28 16:57:00 -0300

    Test: Add test_helper

--- core/order/repo_test.go
+++ core/order/repo_test.go
@@ -2,43 +2,12 @@ package order
 
 import (
 	"context"
-	"database/sql"
 	"testing"
 
 	"github.com/google/uuid"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/assert"
 )
-
-func newOrder(id uuid.UUID, plate string, score int64) *Order {
-	return &Order{
-		ID:    id,
-		Plate: plate,
-		Score: score,
-	}
-}
-
-func getDB(t *testing.T) *sql.DB {
-	db, err := sql.Open("sqlite3", "../../data/restaurant_test.db")
-	assert.Nil(t, err)
-
-	return db
-}
-
-func clearAndClose(db *sql.DB, t *testing.T) {
-	tx, err := db.Begin()
-	assert.Nil(t, err)
-
-	_, err = tx.Exec("delete from orders")
-	assert.Nil(t, err)
-
-	if err != nil {
-		tx.Rollback()
-	}
-
-	tx.Commit()
-	db.Close()
-}
 
 func TestCreateOrder(t *testing.T) {
 	t.Run("success to create a new order", func(t *testing.T) {
